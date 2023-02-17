533b5350dbca059d2f7d114b4ec2a56d274f896d
José <demon.mystery@gmail.com>
2023-02-17 18:48:16 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pokemon-challenge',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
