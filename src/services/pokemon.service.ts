794d97e36e75f11e7e938fdb4bc7507753f0645b
José <demon.mystery@gmail.com>
2023-02-18 01:45:57 -0400

    feat: handling errors in getAll method on pokemons service
    Use the either manager to handle the succeeded or failed responses from the pokeapi

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,15 +1,18 @@
 import { Axios } from "axios";
 import { apiPaths } from "@/constants/api-paths";
-import { transformPokemonListDTO } from "@/models/pokemons.model";
+import { PokemonList, transformPokemonListDTO } from "@/models/pokemons.model";
+import { AsyncEither, Failure, Success } from "@/models/either";
 
 export class PokemonService {
 
     constructor(private axios: Axios) { }
 
-    async getAll(limit: number, offset: number) {
-        return this.axios.get(apiPaths.getAllPokemons(limit, offset))
-            .then((response) => {
-                return transformPokemonListDTO(response.data)
-            })
+    async getAll(limit: number, offset: number): AsyncEither<undefined, PokemonList> {
+        try {
+            const response = await this.axios.get(apiPaths.getAllPokemons(limit, offset))
+            return Success.create(transformPokemonListDTO(response.data))
+        } catch {
+            return Failure.create() 
+        }
     }
 }
--- tests/unit/services/pokemon-service.spec.ts
+++ tests/unit/services/pokemon-service.spec.ts
@@ -1,16 +1,17 @@
+import { Failure, Success } from "@/models/either";
 import { PokemonService } from "@/services/pokemon.service"
 import axios from "axios"
 import MockAdapter from "axios-mock-adapter"
 
-describe("pokemon.services test suite",() => {
+describe("pokemon.services test suite", () => {
 
     let axiosMock: MockAdapter;
-    
+
     beforeEach(() => {
         axiosMock = new MockAdapter(axios)
     })
 
-    it("get all pokemons returns a pokemon list when the request its succesfully", async () => {
+    it("get all pokemons returns a success instance when the request its successfully", async () => {
         axiosMock.onGet().reply(200, {
             count: 1000,
             results: [{
@@ -18,8 +19,22 @@ describe("pokemon.services test suite",() => {
                 url: "https://pokeapi.co/api/v2/pokemon/1/"
             }]
         })
-        const pokemonService = new PokemonService(axios) 
-        expect(await pokemonService.getAll(10, 0)).toEqual({
+        const pokemonService = new PokemonService(axios);
+        const response = await pokemonService.getAll(10, 0)
+        expect(response).toBeInstanceOf(Success)
+    })
+
+    it("get all pokemons returns a pokemon list in the success when the request its succesfully", async () => {
+        axiosMock.onGet().reply(200, {
+            count: 1000,
+            results: [{
+                name: "bulbasaur",
+                url: "https://pokeapi.co/api/v2/pokemon/1/"
+            }]
+        })
+        const pokemonService = new PokemonService(axios);
+        const response = await pokemonService.getAll(10, 0)
+        expect((response as any).result).toEqual({
             count: 1000,
             results: [{
                 id: 1,
@@ -27,6 +42,13 @@ describe("pokemon.services test suite",() => {
                 imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png"
             }]
         })
+    })
+
+    it("get all pokemons returns a failure instance when the endpoint fails", async () => {
+        axiosMock.onGet().reply(500, "Server error")
+        const pokemonService = new PokemonService(axios);
+        const response = await pokemonService.getAll(10, 0)
+        expect(response).toBeInstanceOf(Failure)
     })
 
 })
