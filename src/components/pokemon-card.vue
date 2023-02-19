7848431626086dfaa57f2cf3c09971800a17d122
José <demon.mystery@gmail.com>
2023-02-19 16:46:58 -0400

    feat: :lipstick: pokemon card styleup
    modify the style of poke card to match with the provided one

--- src/components/pokemon-card.vue
+++ src/components/pokemon-card.vue
@@ -1,21 +1,28 @@
 <template>
-  <ion-card>
-    <ion-img :src="pokemon?.imageUrl" alt="pokemon image" />
+  <ion-card @click="onClick">
+    <ion-img :src="pokemon.imageUrl" alt="pokemon image" />
     <ion-card-header>
-      <ion-card-title>{{ pokemon?.name }}</ion-card-title>
-      <ion-card-subtitle># {{ pokemon?.id }}</ion-card-subtitle>
+      <ion-card-title>
+        <h2>{{ pokemonName }}</h2>
+      </ion-card-title>
+      <ion-card-subtitle class="text-s n-500">
+        # {{ pokemonId }}
+      </ion-card-subtitle>
     </ion-card-header>
-    <ion-card-content>
-      <div>
-        Moves <span>{{ pokemon?.moves.length }}</span>
+    <ion-card-content class="ion-no-padding ion-margin-horizontal flex ion-justify-content-start">
+      <div class="text-s ion-margin-end">
+        Moves <span class="n-800">{{ pokemon.moves.length }}</span>
       </div>
-      <div>
-        Experience <span>{{ pokemon?.experience }}</span>
+      <div class="text-s">
+        Experience <span class="n-800">{{ pokemon.experience }}</span>
       </div>
+
     </ion-card-content>
-    <ion-chip v-for="(pokemonType, index) of pokemonTypes" :key="index">
-      <ion-icon :src="pokemonType.icon"></ion-icon>{{ pokemonType.type }}
-    </ion-chip>
+    <div class="ion-margin">
+      <ion-chip class="chip text-s" v-for="(pokemonType, index) of pokemonTypes" :key="index">
+        <ion-icon :src="pokemonType.icon"></ion-icon>{{ pokemonType.type }}
+      </ion-chip>
+    </div>
   </ion-card>
 </template>
 <script lang="ts" setup>
@@ -26,22 +33,44 @@ import {
   IonCardHeader,
   IonCardTitle,
   IonCardSubtitle,
-  IonCardContent
+  IonCardContent,
+  IonIcon
 } from '@ionic/vue';
 import { capitalize, computed } from 'vue';
 import { Pokemon } from '@/models/pokemon.model';
 import { pokemonTypeIcon } from '@/constants/types-icons'
+const props = withDefaults(defineProps<{ pokemon: Pokemon, pokemonCount?: number }>(), {
+  pokemonCount: 0
+})
+const emit = defineEmits(["click"])
 
-const props = defineProps<{ pokemon?: Pokemon }>()
-const pokemonTypes = computed(() => props.pokemon?.types.map((type) => ({
+const pokemonTypes = computed(() => props.pokemon.types.map((type) => ({
   icon: pokemonTypeIcon(type),
   type: capitalize(type)
 })))
+
+const pokemonName = computed(() => capitalize(props.pokemon.name))
+
+const pokemonId = computed(() => {
+  const minZeroPokemonCount = Math.max(0, props.pokemonCount)
+  const pokemonIdLength = props.pokemon.id.toString().length
+  const pokemonCountLength = minZeroPokemonCount.toString().length ?? pokemonIdLength;
+  const missingCharacters = Math.max(0, pokemonCountLength - pokemonIdLength) 
+  return "0".repeat(missingCharacters) + props.pokemon.id
+})
+
+const onClick = () => {
+  emit("click", props.pokemon)
+}
 </script>
 <style>
 ion-img {
   height: 170px;
   margin-top: -10px;
-  background-color: #ECECF2;
+  background-color: var(--neutral-100);
+}
+
+ion-card {
+  border-radius: 16px;
 }
 </style>
--- tests/unit/components/pokemon-card.spec.ts
+++ tests/unit/components/pokemon-card.spec.ts
@@ -17,8 +17,8 @@ describe("Test suite for pokemon card component", () => {
         expect(pokemonCard.find("ion-img").element.src).toBe(dummyBulbasaurParsed.imageUrl)
     })
 
-    it("The element show the pokemon name", () => {
-        expect(pokemonCard.text()).toContain(dummyBulbasaurParsed.name)
+    it("The element show the pokemon name capitalized", () => {
+        expect(pokemonCard.text()).toContain(capitalize(dummyBulbasaurParsed.name))
     })
 
     it("The card shows the pokemon id", () => {
@@ -28,7 +28,6 @@ describe("Test suite for pokemon card component", () => {
     it("The card render the moves quantity", () => {
         expect(pokemonCard.text()).toContain(`Moves ${dummyBulbasaurParsed.moves.length}`)
     })
-
     it("The card render the experience", () => {
         expect(pokemonCard.text()).toContain(`Experience ${dummyBulbasaurParsed.experience}`)
     })
@@ -48,6 +47,37 @@ describe("Test suite for pokemon card component", () => {
             const iconPath = pokemonTypeIcon(type)
             expect(pokemonCard.findAll("ion-icon")[i].element.src).toBe(iconPath)    
         })
+    })
+
+    it("pokemon card emit event when its clicked", () => {
+        pokemonCard.trigger("click")
+        expect(pokemonCard.emitted("click")).toHaveLength(1)
+    })
+
+    it("pokemon card emit the pokemon when user press it", () => {
+        pokemonCard.trigger("click")
+        const result = pokemonCard.emitted("click")
+        expect(result![0][0]).toEqual(dummyBulbasaurParsed)
+    })
+
+    it("pokemon modify the id adding left 0 when pokemonCount its setted", () => {
+        pokemonCard = mount(PokemonCard, {
+            props: {
+                pokemon: dummyBulbasaurParsed,
+                pokemonCount: 1000
+            }
+        })
+        expect(pokemonCard.html()).toContain("# 0001")
+    })
+
+    it("pokemon returns the same pokemon id whe a negative pokemonCount its provided", () => {
+        pokemonCard = mount(PokemonCard, {
+            props: {
+                pokemon: dummyBulbasaurParsed,
+                pokemonCount: -1000
+            }
+        })
+        expect(pokemonCard.html()).toContain("# 1")
     })
 
 })
