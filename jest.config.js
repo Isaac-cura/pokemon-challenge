c628799da5b4517544c061e37999ebae47a0e873
José <demon.mystery@gmail.com>
2023-02-20 14:57:06 -0400

    chore: modify the jest config to shows coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  transformIgnorePatterns: ['/node_modules/(?!@ionic/vue|@ionic/vue-router|@ionic/core|@stencil/core|ionicons|axios)']
+  transformIgnorePatterns: ['/node_modules/(?!@ionic/vue|@ionic/vue-router|@ionic/core|@stencil/core|ionicons|axios)'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,vue}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],  
+  coverageReporters: ['clover', 'json', 'lcov', ['text', {skipFull: true}]],
 }
