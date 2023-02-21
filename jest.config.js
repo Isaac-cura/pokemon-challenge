3c2e48b2a32c4fca18c866d7fed649df6fd40142
José <demon.mystery@gmail.com>
2023-02-20 21:05:28 -0400

    chore: :zap: remove coverage check in order to improve the performance of tests
    the coverage takes long time running, that retrase development times i will activate it to builds but not to develop

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,4 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transformIgnorePatterns: ['/node_modules/(?!@ionic/vue|@ionic/vue-router|@ionic/core|@stencil/core|ionicons|axios)'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.{ts,vue}',
-    '!**/node_modules/**',
-    '!**/vendor/**',
-  ],  
-  coverageReporters: ['clover', 'json', 'lcov', ['text', {skipFull: true}]],
 }
